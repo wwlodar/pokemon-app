47e781a3ffd66d61c5ae927b7d0aeb885e6eaff9
wwlodar <werwlodarczyk@wp.pl>
2020-12-15 22:15:22 +0100

    Pokemon-app

--- basic.py
+++ basic.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('basic.html')
+    return render_template('home.html')
+
+@app.route('/pokemon/<name>')
+def poke_name(name):
+    return render_template('pokemon.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug = True)
