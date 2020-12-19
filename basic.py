d40f11f215b063b63cfc023423fdb49e2f79197e
wwlodar <werwlodarczyk@wp.pl>
2020-12-19 21:45:37 +0100

    .

--- basic.py
+++ basic.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/pokemon/<name>')
-def poke_name(name):
-    return render_template('pokemon.html', name=name)
 
+@app.route('/pokemon')
+def poke_name():
+    pokemon_name = request.args.get('pname')
+    lower_letter = any(c.islower() for c in pokemon_name)
+    return render_template('pokemon.html',pokemon_name=pokemon_name, lower = lower_letter)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 if __name__ == '__main__':
     app.run(debug = True)
 ##base
