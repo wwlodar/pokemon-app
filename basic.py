8b12ed73f1f7a9abb29c327f24086f67f391bc54
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-01-11 22:06:23 +0100

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-## work on adding more pokemons....
 @app.route('/pokemon/<name>')
 def poke_name():
-    pokemon_name = request.args.get('pname')
-    lower_letter = any(c.islower() for c in pokemon_name)
-    return render_template('pokemon.html',pokemon_name=pokemon_name, lower = lower_letter)
+    pname = request.args.get('pname')
+    pname = pname.lower()
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(pname))
+    lower_letter = any(c.islower() for c in pname)
+    return render_template('pokemon.html',pname=pname, lower = lower_letter, response=response)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 if __name__ == '__main__':
     app.run(debug = True)
-##base
