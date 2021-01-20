ef5a97d2a6f96ea69b2190ab87eb3c1d25a618a9
wwlodar <werwlodarczyk@wp.pl>
2021-01-20 23:32:59 +0100

    pokemon-app

--- basic.py
+++ basic.py
@@ -1,34 +1,40 @@
 from flask import Flask, render_template, request
 import requests
-import json
+
+from dictor import dictor
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-def get_pokemon():
-    global pname
-    pname = request.args.get('pname')
-    pname = pname.lower()
+def reading_pokemon_data():
+    global anali
+    global response
     response = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(pname))
-    global pokemon_data
     pokemon_data = response.json()
-    global ability
-    ability = pokemon_data['abilities']
-    global lower_letter
-    lower_letter = any(c.islower() for c in pname)
+    anali = dictor(pokemon_data, 'types.0.type.name')
 
 
 @app.route('/pokemon')
 def poke_name():
-    get_pokemon()
-    type = pokemon_data['types']
-    return render_template('pokemon.html', pname=pname, lower=lower_letter, ability=ability,type=type)
+    global pname
+    pname = request.args.get('pname').lower()
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/' + str(pname))
+    if response.status_code == 200 and len(pname) != 0:
+        reading_pokemon_data()
+        return render_template('pokemon.html', pname=pname, anali=anali)
+    elif response.status_code == 200 and len(pname) == 0:
+        return render_template('home.html')
+    else:
+        return render_template('error.html', pname=pname)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
-##base
+    app.run(debug=True)
